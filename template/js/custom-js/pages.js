5501d83dade32542e8879540b8e5be629fac7ab5
matheusgnreis <matheus@e-com.club>
2023-03-04 23:35:06 -0300

    chore: add event listener

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,10 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
 setTimeout(() => {
-    document.querySelector('button.close').addEventListener('click', () => {
-        console.log('fui')
-        sessionStorage.setItem('modal', 'close')
-        return document.querySelector('button.close').removeEventListener('click', null, true)
-    })
+    document.getElementById('closeModal').addEventListener('click', () => sessionStorage.setItem('modal', 'close'))
     if (!sessionStorage.getItem('modal')) {
         $('#exampleModalLong').modal('show')
     }
