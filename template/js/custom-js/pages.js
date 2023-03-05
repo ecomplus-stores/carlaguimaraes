b2bd7698f4f70d178ccfd0d35c20aa4a8867f45b
matheusgnreis <matheus@e-com.club>
2023-03-04 23:05:58 -0300

    chore: insert modal

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,11 @@
 // Add your custom JavaScript for storefront pages here.
+setTimeout(() => {
+    document.querySelector('button.close').addEventListener('click', () => {
+        console.log('fui')
+        sessionStorage.setItem('modal', 'close')
+        return document.querySelector('button.close').removeEventListener('click', null, true)
+    })
+    if (!sessionStorage.getItem('modal')) {
+        $('#exampleModalLong').modal('show')
+    }
+}, 1200)
