b0c16fce88f45be5c76c9728922c102ebb516652
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-09-05 11:54:24 -0300

    chore: insert grid avaliacoes

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
+      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html')
     }
   }
